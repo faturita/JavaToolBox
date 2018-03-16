110ef9074ab9873de384eac86c3f3b145b65adde
faturita <rramele@gmail.com>
2018-03-16 11:21:11 -0300

    Adding some comments

--- src/org/security/DESCrypt.java
+++ src/org/security/DESCrypt.java
@@ -1,14 +1,17 @@
 package org.security;
 import java.util.BitSet;
 
+import java.text.NumberFormat;
+
+
 /**
  * Created by IntelliJ IDEA. User: vexrarod Date: 04/09/2003 Time: 09:49:19 To
  * change this template use Options | File Templates.
- * 
+ *
  * Local implementation of Simple DES algorithm.
- * 
+ *
  * Very simple and fast !
- * 
+ *
  * <a href=
  * "http://www.mapleapps.com/powertools/cryptography/HTML/DES-KeyExpansion.html"
  * /> <a href=
@@ -96,6 +99,10 @@ class Bits extends BitSet {
         }
 
         return str.toString();
+    }
+
+    public String toHex() {
+      return String.format("0x%08X", toLong());
     }
 
     public Bits add(Bits b) {
@@ -226,6 +233,7 @@ public class DESCrypt {
         for (int i = 0; i < s.length; i++) {
             for (int j = 0; j < 8; j++) {
                 boolean bSet = ((s[i] >> j) & 0x01) == 1 ? true : false;
+                System.out.println(plaintext[i]);
                 plaintext[i].set(i * 8 + j, bSet);
             }
             cipher[i] = t.des(plaintext[i], key, false);
@@ -234,31 +242,28 @@ public class DESCrypt {
     }
 
     public static void main(String args[]) {
-        streamcompress(args[0], args[1]);
-    }
-
-    public static void maind(String args[]) {
         DESCrypt t = new DESCrypt();
         Bits plaintext = new Bits(64);
         Bits key = new Bits(64);
         long a, b;
 
         plaintext.load(0x0123456789abcdefL);
-        System.out.println(plaintext + "-Plaintext:" + plaintext.toLong());
+        System.out.println(plaintext + "-Plaintext:" + plaintext.toLong()+":"+String.format("0x%08X", plaintext.toLong()));
         a = plaintext.toLong();
 
         key.load(0x133457799bbcdff3L);
-        System.out.println(key + "-Key      :" + key.toLong());
+        key.load(0x0101010101010101L);
+        System.out.println(key + "-Key      :" + key.toLong() + ":" + key.toHex());
 
         Bits out = t.des(plaintext, key, false);
-        System.out.println(out + "-Cipher   :" + out.toLong());
+        System.out.println(out + "-Cipher   :" + out.toLong()+ ":" + out.toHex());
 
         Bits in = t.des(out, key, true);
-        System.out.println(in + "-Plaintext:" + in.toLong());
+        System.out.println(in + "-Plaintext:" + in.toLong()+ ":" + in.toHex());
         b = in.toLong();
 
         if (a == b)
-            System.out.println("Perfect match.!");
+            System.out.println("Encryption Scheme Ok!");
     }
 
     public Bits KS(int i, Bits Key) {
@@ -345,6 +350,12 @@ public class DESCrypt {
         return fOut;
     }
 
+    /**
+     * This is the code of the entire algorithm.
+     *
+     * The flag allows to invert the last permutation which determines if we
+     * are ciphering or deciphering.
+     **/
     public Bits des(Bits plaintext, Bits key, boolean bDecrypt) {
         Bits cipher = new Bits(64);
 
@@ -404,105 +415,104 @@ public class DESCrypt {
     }
 }
 /*
- * 
+ *
  * IP 58 50 42 34 26 18 10 2 60 52 44 36 28 20 12 4 62 54 46 38 30 22 14 6 64 56
  * 48 40 32 24 16 8 57 49 41 33 25 17 9 1 59 51 43 35 27 19 11 3 61 53 45 37 29
  * 21 13 5 63 55 47 39 31 23 15 7
- * 
- * 
- * public static void
- * 
+ *
+ *
+ *
  * IP-1
- * 
+ *
  * 40 8 48 16 56 24 64 32 39 7 47 15 55 23 63 31 38 6 46 14 54 22 62 30 37 5 45
  * 13 53 21 61 29 36 4 44 12 52 20 60 28 35 3 43 11 51 19 59 27 34 2 42 10 50 18
  * 58 26 33 1 41 9 49 17 57 25
- * 
- * 
- * 
+ *
+ *
+ *
  * E BIT-SELECTION TABLE
- * 
+ *
  * 32 1 2 3 4 5 4 5 6 7 8 9 8 9 10 11 12 13 12 13 14 15 16 17 16 17 18 19 20 21
  * 20 21 22 23 24 25 24 25 26 27 28 29 28 29 30 31 32 1
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * P
- * 
- * 
+ *
+ *
  * 16 7 20 21 29 12 28 17 1 15 23 26 5 18 31 10 2 8 24 14 32 27 3 9 19 13 30 6
  * 22 11 4 25
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * S1
- * 
+ *
  * 14 4 13 1 2 15 11 8 3 10 6 12 5 9 0 7 O 15 7 4 14 2 13 1 10 6 12 11 9 5 3 8 4
  * 1 14 8 13 6 2 11 15 12 9 7 3 10 5 0 15 12 8 2 4 9 1 7 5 11 3 14 10 O 6 13
- * 
+ *
  * S2
- * 
+ *
  * 15 1 8 14 6 11 3 4 9 7 2 13 12 O 5 10 3 13 4 7 15 2 8 14 12 0 1 10 6 9 11 5 0
  * 14 7 11 10 4 13 1 5 8 12 6 9 3 2 15 13 8 10 1 3 15 4 2 11 6 7 12 0 5 14 9
- * 
+ *
  * S3
- * 
+ *
  * 10 0 9 14 6 3 15 5 1 13 12 7 11 4 2 8 13 7 O 9 3 4 6 10 2 8 5 14 12 11 15 1
  * 13 6 4 9 8 15 3 0 11 1 2 12 5 10 14 7 1 10 13 0 6 9 8 7 4 15 14 3 11 5 2 12
- * 
+ *
  * S4
- * 
+ *
  * 7 13 14 3 0 6 9 10 1 2 8 5 11 12 4 15 13 8 11 5 6 15 O 3 4 7 2 12 1 10 14 9
  * 10 6 9 0 12 11 7 13 15 1 3 14 5 2 8 4 3 15 O 6 10 1 13 8 9 4 5 11 12 7 2 14
- * 
- * 
+ *
+ *
  * S5
- * 
+ *
  * 2 12 4 1 7 10 11 6 8 5 3 15 13 O 14 9 14 11 2 12 4 7 13 1 5 0 15 10 3 9 8 6 4
  * 2 1 11 10 13 7 8 15 9 12 5 6 3 O 14 11 8 12 7 1 14 2 13 6 15 O 9 10 4 5 3
- * 
- * 
+ *
+ *
  * S6
- * 
+ *
  * 12 1 10 15 9 2 6 8 O 13 3 4 14 7 5 11 10 15 4 2 7 12 9 5 6 1 13 14 O 11 3 8 9
  * 14 15 5 2 8 12 3 7 0 4 10 1 13 11 6 4 3 2 12 9 5 15 10 11 14 1 7 6 0 8 13
- * 
+ *
  * S7
- * 
+ *
  * 4 11 2 14 15 0 8 13 3 12 9 7 5 10 6 1 13 0 11 7 4 9 1 10 14 3 5 12 2 15 8 6 1
  * 4 11 13 12 3 7 14 10 15 6 8 0 5 9 2 6 11 13 8 1 4 10 7 9 5 0 15 14 2 3 12
- * 
- * 
+ *
+ *
  * S8
- * 
+ *
  * 13 2 8 4 6 15 11 1 10 9 3 14 5 0 12 7 1 15 13 8 10 3 7 4 12 5 6 11 0 14 9 2 7
  * 11 4 1 9 12 14 2 0 6 10 13 15 3 5 8 2 1 14 7 4 10 8 13 15 12 9 0 3 5 6 11
- * 
+ *
  * The primitive function P is: 16 7 20 21 29 12 28 17 1 15 23 26 5 18 31 10 2 8
  * 24 14 32 27 3 9 19 13 30 6 22 11 4 25
- * 
- * 
- * 
+ *
+ *
+ *
  * PC-1
- * 
- * 
+ *
+ *
  * 57 49 41 33 25 17 9 1 58 50 42 34 26 18 10 2 59 51 43 35 27 19 11 3 60 52 44
  * 36
- * 
+ *
  * 63 55 47 39 31 23 15 7 62 54 46 38 30 22 14 6 61 53 45 37 29 21 13 5 28 20 12
  * 4
- * 
- * 
+ *
+ *
  * Iteration Number of Number Left Shifts
- * 
+ *
  * 1 1 2 1 3 2 4 2 5 2 6 2 7 2 8 2 9 1 10 2 11 2 12 2 13 2 14 2 15 2 16 1
- * 
- * 
+ *
+ *
  * PC-2
- * 
- * 
+ *
+ *
  * 14 17 11 24 1 5 3 28 15 6 21 10 23 19 12 4 26 8 16 7 27 20 13 2 41 52 31 37
  * 47 55 30 40 51 45 33 48 44 49 39 56 34 53 46 42 50 36 29 32
  */
